9f27d4bc3a547eddb0b820e272c87f49c9229eb3
Aded <joaopedro_aded@hotmail.com>
2023-09-02 19:39:28 -0300

    feat: finish mobile and tablet query

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../../assets/WeFashion2.svg'
 import "./Navbar.scss"
+import iconNavbar from './../../assets/iconNavbar.svg'
 
 const Navbar = () => {
+  const [ menuView, setMenuView ] = useState(false);
+
+  const showMenu = () => setMenuView(!menuView); 
+
+
+  // quando carrega a página no tamanho certo ocorre tudo ok, porém erro de logica quando altera o tamanho da página para celular - desktop
+  const canShow = () => {
+    const largura = window.innerWidth;
+    if(largura>390){
+      return true;
+    }
+    if(menuView == true) {
+      return true;
+    }
+    return false
+  }
+
+
   return (
     <div className='navbar'>
-        <div className='navbar__img'>
+        <div className='navbar__logo'>
             <img src={logo} alt="ErroLogo-WeFashion2" />
         </div>
-        <div className='navbar__menu'>
-            <ul>
-                <li>Inicio</li>
-                <li>Cursos</li>
-                <li>Sobre</li>
-                <li>Contato</li>
-            </ul>
-            <button>Quero ser um aluno</button>
-        </div>
+        
+        {   canShow() && (
+          <div className='navbar__menu'>
+              <ul>
+                  <li><a href="">Inicio</a></li>
+                  <div className='line'></div>
+                  <li><a href="">Cursos</a></li>
+                  <div className='line'></div>
+                  <li><a href="">Sobre</a></li>
+                  <div className='line'></div>
+                  <li><a href="">Contato</a></li>
+                  <div className='line'></div>
+              </ul>
+              <button>Quero ser um aluno</button>
+          </div>
+          )
+        }
+        <img className='navbar__icon' onClick={showMenu} src={iconNavbar} alt="ErrorIcon-Menu" />
     </div>
   )
 }
