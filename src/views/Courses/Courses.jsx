6a2d30387ec8071f72571272400e1202d5217fe9
RafaelLoureiro <rafanetmello@gmail.com>
2023-09-04 18:14:05 -0300

    cards update icons and image, create new folder access -svg -jpg

--- src/views/Courses/Courses.jsx
+++ src/views/Courses/Courses.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import "./Courses.scss"
+import "./Courses.scss";
+import starIcon from  "/src/assets/icons/star.svg";
+import timeIcon from  "/src/assets/icons/time.svg";
+import peopleIcon from  "/src/assets/icons/people.svg";
+import mapIcon from  "/src/assets/icons/map.svg";
+import modelagemImg from "/src/assets/images/modelagem.jpg"
 export default class Courses extends Component {
     render() {
         return (
@@ -11,24 +16,38 @@ export default class Courses extends Component {
                 <div className='courses-Container__Cards'>
                     <div className='card-Box1'>
                         <div className='card-Box1__Folder'>
-                            <img src="" alt="" className='card-Box1__Img' />
+                            <img src={modelagemImg} alt="" className='card-Box1__Img' />
                             <button className='card-Box1__Price'>R$0,00</button>
                         </div>
-                        <div className='card-Box1_Assessment'>
-                            <p>++++ 4.5  (45 avaliações)</p>
+                        <div className='card-Box1__Assessment'>
+                            <p><img src={starIcon}/><img src={starIcon}/><img src={starIcon}/><img src={starIcon}/><img src={starIcon}/> 4.5  (45 avaliações)</p>
                             <h3>Curso Tecido Plano Feminino</h3>
-                            <p> (8)icon (8)icon (8)</p>
+                            <p> <img src={timeIcon}/> 12Meses <img src={peopleIcon}/>7alunos  <img src={mapIcon}/></p>
                         </div>
                     </div>
-                    <div id='card-Box2'>
-                        <div className='card-Box2__Description'>
-                            <h1>R$0,00</h1>
-                            <h3>Curso Tecido Plano Feminino</h3>
-                            <p> (8)icon </p>
-                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                    <div className='card-Box1'>
+                        <div className='card-Box1__Folder'>
+                            <img src={modelagemImg} alt="" className='card-Box1__Img' />
+                            <button className='card-Box1__Price'>R$0,00</button>
                         </div>
-                        <button className='card-Box1__Price2'></button>
+                        <div className='card-Box1__Assessment'>
+                            <p><img src={starIcon}/><img src={starIcon}/><img src={starIcon}/><img src={starIcon}/><img src={starIcon}/> 4.5  (45 avaliações)</p>
+                            <h3>Curso Modelagem</h3>
+                            <p> <img src={timeIcon}/> 12 Meses <img src={peopleIcon}/> 15alunos  <img src={mapIcon}/></p>
+                        </div>
                     </div>
+                    <div className='card-Box1'>
+                        <div className='card-Box1__Folder'>
+                            <img src={modelagemImg} alt="" className='card-Box1__Img' />
+                            <button className='card-Box1__Price'>R$0,00</button>
+                        </div>
+                        <div className='card-Box1__Assessment'>
+                            <p><img src={starIcon}/><img src={starIcon}/><img src={starIcon}/><img src={starIcon}/><img src={starIcon}/> 4.5  (45 avaliações)</p>
+                            <h3>Curso Tecido Plano Feminino</h3>
+                            <p> <img src={timeIcon}/> 6 Meses <img src={peopleIcon}/> 35 alunos  <img src={mapIcon}/></p>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         )
