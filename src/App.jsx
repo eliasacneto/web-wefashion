5bfcbf73a65947e86b0ce04d94e2fa4cf7809a8e
Hugo Zeymer Auad <125771692+HugoZAuad@users.noreply.github.com>
2023-09-04 06:51:20 -0300

    Our_school
    Criação da parte da nossa escola, adicionada as informações e posicionado a foto, ficando pendente somente a rotação das imagens.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./views/Footer/Footer";
+import Nescola from "./views/Nescola/Nescola";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Nescola />
       <Footer />
     </>
   );
