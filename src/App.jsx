5d38331fb56046a79881064ed9e5c06da771ce55
Marcelo <marcelosifr@gmail.com>
2023-09-08 17:17:07 -0300

    Adicionando Mock
    Adicionando mock de cursos disponíveis

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./views/Footer/Footer";
 import Courses from "./views/Courses/Courses";
+import { PRODUCTS_MOCK } from "./mock/product.mock";
+
 function App() {
   return (
     <>
