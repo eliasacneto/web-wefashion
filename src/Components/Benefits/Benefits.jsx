1b435e84fe1e8a401dff56dceb41aab113ccb838
Aded <joaopedro_aded@hotmail.com>
2023-09-11 15:13:07 -0300

    feat: finish component benefits and responsive

--- src/Components/Benefits/Benefits.jsx
+++ src/Components/Benefits/Benefits.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import yellowImg from './../../assets/WeFashion-img-yellow.svg'
 import iconCheck from './../../assets/icon-check.svg'
+import whattsIcon from './../../assets/whatsapp.png'
 import './Benefits.scss'
 
 const Benefits = () => {
   return (
     <div className='benefits'>
         <div className='benefits__content'> 
-            <h1>titulos beeeem legal</h1>
+            <h1>Titulo beeeem legal</h1>
             <div className='benefits__content-benefits'>
                 <img src={iconCheck} alt="" /> <h2>Um professor especialista em modelagem</h2>
             </div>
@@ -15,7 +16,7 @@ const Benefits = () => {
                 <img src={iconCheck} alt="" /> <h2>Um professor especialista em corte e costura</h2>
             </div>
             <div className='benefits__content-benefits'>
-                <img src={iconCheck} alt="" /> <h2>Maquinário e mesa para treino agendado</h2>
+                <img src={iconCheck} alt="" /> <h2>Maquinárino e mesa para treino agendado</h2>
             </div>
             <div className='benefits__content-benefits'>
                 <img src={iconCheck} alt="" /> <h2>Temos lojas com todo material necessário para o curso</h2>
@@ -23,7 +24,7 @@ const Benefits = () => {
 
             <h4>Inscreva-se agora mesmo e comeca a estudar através dos nossos cursos gratuitos disponíveis!</h4>
             
-            <button className='btn-inscrever'> Whatts </button>
+            <button className='btn-whatts'><img src={whattsIcon} alt="" />Fale conosco </button>
         </div>
 
         <div className='benefits__img'>
