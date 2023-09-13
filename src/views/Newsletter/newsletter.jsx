ee992f3088f07b5c11fb98a34fe1c98c1b3f3251
Rodrigo Brandão <meiramusic@Outlook.com>
2023-09-13 16:55:43 -0300

    Newsletter section  done ✅

--- src/views/Newsletter/newsletter.jsx
+++ src/views/Newsletter/newsletter.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import wefashionPic from "./../../assets/Wefashion-preto.svg";
-import "./newsletter.scss";
+import React, { useState } from 'react';
+import wefashionPic from './../../assets/Wefashion-preto.svg';
+import './newsletter.scss';
 
 const Newsletter = () => {
+  const [email, setEmail] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
     <div className="newsletter">
       <div className="side-content">
-        <h1 className='newsletter_title'>
-          Seja você também um aluno <span className='yellow-text'>WeFashion</span>
+        <h1 className="newsletter_title">
+          Seja você também um aluno <span className="yellow-text">WeFashion</span>
         </h1>
-        <p className='newsletter_text'>
+        <p className="newsletter_text">
           Lorem Ipsum has been the industry's standard dummy text ever since the
           1500s, when an unknown printer took a galley of type and scrambled it
           to make a type specimen book.
         </p>
-        <input className='newsletter-input' type="text" />
-        <label className='input-placeholder'>Digite o seu melhor e-mail</label>
+        <div className="input-container">
+          <input
+            className={email ? 'newsletter-input active' : 'newsletter-input'}
+            type="text"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <label className="input-placeholder">Digite o seu melhor e-mail</label>
+          <button className="subscribe-button">Enviar</button>
+        </div>
       </div>
       <div className="newsletter_img">
         <img src={wefashionPic} alt="Erro-img" />
@@ -24,4 +38,4 @@ const Newsletter = () => {
   );
 };
 
-export default Newsletter
+export default Newsletter;
