31902ed77c3d47bd025bdd766b11b0319722d28b
Rodrigo Brandão <meiramusic@Outlook.com>
2023-09-13 17:02:43 -0300

    Newsletter section done ✅

--- src/views/Newsletter/newsletter.jsx
+++ src/views/Newsletter/newsletter.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import wefashionPic from './../../assets/Wefashion-preto.svg';
-import './newsletter.scss';
+import React, { useState } from "react";
+import wefashionPic from "./../../assets/Wefashion-preto.svg";
+import "./newsletter.scss";
 
 const Newsletter = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -13,21 +13,24 @@ const Newsletter = () => {
     <div className="newsletter">
       <div className="side-content">
         <h1 className="newsletter_title">
-          Seja você também um aluno <span className="yellow-text">WeFashion</span>
+          Seja você também um aluno{" "}
+          <span className="yellow-text">WeFashion</span>
         </h1>
         <p className="newsletter_text">
-          Lorem Ipsum has been the industry's standard dummy text ever since the
-          1500s, when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book.
+          Preencha o campo abaixo com seu endereço de e-mail e comece sua
+          jornada na WeFashion. Este é o primeiro passo para se tornar um aluno
+          WeFashion e mergulhar no mundo emocionante da moda!
         </p>
         <div className="input-container">
           <input
-            className={email ? 'newsletter-input active' : 'newsletter-input'}
+            className={email ? "newsletter-input active" : "newsletter-input"}
             type="text"
             value={email}
             onChange={handleEmailChange}
           />
-          <label className="input-placeholder">Digite o seu melhor e-mail</label>
+          <label className="input-placeholder">
+            Digite o seu melhor e-mail
+          </label>
           <button className="subscribe-button">Enviar</button>
         </div>
       </div>
