d665c7910204486e441062cb36bc470663a9a470
Hugo Zeymer Auad <125771692+HugoZAuad@users.noreply.github.com>
2023-09-13 22:13:40 -0300

    New commit - 13/09 (night)
    Movimentação da pasta Nescola e seus arquivos para a pasta components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Benefits from "./Components/Benefits/Benefits";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./views/Footer/Footer";
-import Nescola from "./views/Nescola/Nescola";
+import Nescola from "./Components/Nescola/Nescola";
 import Testimonials from "./views/Testimonials/Testimonials";
 
 
