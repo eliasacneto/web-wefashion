62d44683d78d57da64c9a86cd13240f673760eb9
Marcelo <marcelosifr@gmail.com>
2023-09-22 01:37:19 -0300

    Atualizando branch
    Criado carrossel de cursos na pagina inicial
    Estilizada a segunda view: view de detalhes de curso

--- src/views/Courses/Courses.jsx
+++ src/views/Courses/Courses.jsx
@@ -2,6 +2,13 @@ import React, { Component, useState } from 'react';
 import "./Courses.scss";
 import { itens } from '../../mock/product.mock'
 import Cards from '../../Components/Cards/Cards';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { EffectFade } from 'swiper/modules';
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 
 export default function Courses() {
@@ -14,14 +21,25 @@ export default function Courses() {
                     <h1>Os melhores cursos</h1>
                     <h2>Cursos em destaque</h2>
                 </div>
+                
                 <div className='courses-Container__Cards'>
-                    {products.map((product) => (
-                        <Cards data={product} key={product.id} />
-                    ))
-                    }
-                    
+                    <Swiper className='courses-Container__Cards-swiper'
+                    slidesPerView={3}
+                    loop
+                    navigation
+                    modules={[ Navigation ]}
+                    >
+                        {products.map((product) => (
+                            <SwiperSlide className='courses-Container__Cards-swiper-slide' key={product.id}>
+                                <Cards data={product} key={product.id} />
+                            </SwiperSlide>
+                            ))
+                        }
+                    </Swiper>
                 </div>
+                
             </div>
+            
         );
     
 }
